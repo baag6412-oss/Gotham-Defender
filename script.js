df25409c3a1d1a15e924f90a1a615cb49d40851f
baag6412 <baag6412@gmail.com>
2025-11-02 12:17:12 +0800

    final

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let currentDoor = { joker: null, batman: null };
+let score = 0;
+let isGameOver = false;
 
 document.addEventListener("DOMContentLoaded", () => {
     const board = document.getElementById("board");
@@ -6,20 +8,22 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let i = 0; i < 9; i++) {
         const door = document.createElement("div");
         door.id = i.toString();
+        door.addEventListener("click", selectDoor);
         board.appendChild(door);
     }
+
+    setCharacter("batman");
 
     setInterval(() => setCharacter("joker"), 1500);
     setInterval(() => setCharacter("batman"), 2000);
 });
 
 const getRandomDoorId = () => Math.floor(Math.random() * 9).toString();
-
 const setCharacter = (character) => {
+    if (isGameOver) return;
     const randomDoorId = getRandomDoorId();
 
     if (isDoorOccupied(randomDoorId)) return;
-
     const randomDoor = document.getElementById(randomDoorId);
     console.log(randomDoor);
     const img = document.createElement("img");
@@ -29,12 +33,45 @@ const setCharacter = (character) => {
     currentDoor[character] = randomDoor;
     setTimeout(() => clearDoor(character), 1000);
 };
-
 const isDoorOccupied = (randomDoorId) =>
     currentDoor.joker?.id === randomDoorId ||
     currentDoor.batman?.id === randomDoorId;
+
 const clearDoor = (character) => {
     if (currentDoor[character]) {
         currentDoor[character].innerHTML = "";
     }
+};
+const selectDoor = (e) => {
+    const selectedDoor = e.target;
+    if (selectedDoor.children.length === 0) return;
+    if (selectedDoor === currentDoor.joker) {
+        updateScore();
+        clearDoor("joker");
+    }
+    if (selectedDoor === currentDoor.batman) {
+        isGameOver = true;
+        const scoreText = document.getElementById("score");
+        scoreText.textContent = `GAME OVER: ${score}`;
+
+        showRestartButton();
+    }
+};
+const updateScore = () => {
+    score += 10;
+    const scoreText = document.getElementById("score");
+    scoreText.textContent = score.toString();
+};
+const showRestartButton = () => {
+    const restart = document.getElementById("restart");
+    restart.classList.add("active");
+    restart.addEventListener("click", restartGame);
+};
+const restartGame = () => {
+    isGameOver = false;
+    score = 0;
+    const scoreText = document.getElementById("score");
+    scoreText.textContent = score.toString();
+    const restart = document.getElementById("restart");
+    restart.classList.remove("active");
 };
